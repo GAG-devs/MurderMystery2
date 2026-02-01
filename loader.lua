c36041b899e0864762ebec9e69687777e8d46a6c
GrowAGarden <amiernaifcammah@gmail.com>
2026-02-01 17:39:42 +0800

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/eC0ztEBd/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/2DTkNKca/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua", true))()
